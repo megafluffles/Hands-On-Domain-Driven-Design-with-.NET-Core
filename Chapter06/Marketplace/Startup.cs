139ece6224610bfa7c806ad7da73007b2395fee6
Kaj Bromose <kbr@ucl.dk>
2020-09-15 10:09:59 +0200

    Ch06->3-1

--- Chapter06/Marketplace/Startup.cs
+++ Chapter06/Marketplace/Startup.cs
@@ -5,10 +5,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Raven.Client;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Raven.Client.Documents;
-using Raven.Client.Documents.Session;
-using Swashbuckle.AspNetCore.Swagger;
 
 // ReSharper disable UnusedMember.Global
 
@@ -16,28 +15,28 @@ namespace Marketplace
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment environment, IConfiguration configuration)
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
             Environment = environment;
             Configuration = configuration;
         }
 
         private IConfiguration Configuration { get; }
-        private IHostingEnvironment Environment { get; }
+        private IWebHostEnvironment Environment { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             var store = new DocumentStore
-              {
-                  Urls = new[] {"http://localhost:8080"},
-                  Database = "Marketplace_Chapter6",
-                  Conventions =
-                  {
-                      FindIdentityProperty = m => m.Name == "_databaseId"
-                  }
-              };
+            {
+                Urls = new[] {"http://localhost:8080"},
+                Database = "Marketplace_Chapter6",
+                Conventions =
+                {
+                    FindIdentityProperty = m => m.Name == "_databaseId"
+                }
+            };
             store.Conventions.RegisterAsyncIdConvention<ClassifiedAd>(
-                (dbName, entity) => Task.FromResult("ClassifiedAd/" + entity.Id.ToString()));
+                (dbName, entity) => Task.FromResult("ClassifiedAd/" + entity.Id));
             store.Initialize();
 
             services.AddTransient(c => store.OpenAsyncSession());
@@ -49,7 +48,7 @@ namespace Marketplace
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1",
-                    new Info
+                    new OpenApiInfo
                     {
                         Title = "ClassifiedAds",
                         Version = "v1"
@@ -57,17 +56,22 @@ namespace Marketplace
             });
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
-            app.UseMvcWithDefaultRoute();
+
             app.UseSwagger();
             app.UseSwaggerUI(c =>
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClassifiedAds v1"));
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ClassifiedAds v1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
